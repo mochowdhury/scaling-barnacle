ee830b89ceec338ff60882f2303add05b9a9565a
mochowdhury <mochowdhury@users.noreply.github.com>
2021-12-21 18:24:25 -0500

    JS pseudo code

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+/*
+Instructions
+ The car will drive until an obstacle appears.
+ Answer the question
+        if successful, game continues
+        if unsuccessful, game continues with a strike
+        three strikes and you're out.
+
+Time.now() - Time.now() (5 seconds gap)
+ */
